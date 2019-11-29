4455c3356fab40d1600c6ec8d0e8b9c3b2eec77a
Erdem Isguder <erdem.isguder@dk.akbank.local>
2019-11-29 17:19:31 +0300

    push

---
+++ SideAttack/Assets/Scripts/Rogue/InputComponent.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using UnityEngine;
+
+public class InputComponent : MonoBehaviour
+{
+    public event Action OnRightClicked = delegate { };
+    public event Action OnLeftClicked = delegate { };
+   
+    void Update()
+    {
+        InputUpdateOnEditor();
+    }
+
+    private void InputUpdateOnEditor()
+    {
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            Debug.Log("LEFT CLICKED");
+        }
+        else if (Input.GetKeyDown(KeyCode.D))
+        {
+            Debug.Log("RIGHT CLICKED");
+        }
+    }
+
+    private void InputUpdateOnMobile()
+    {
+        int touchCounts = Input.touchCount;
+        if (touchCounts > 0)
+        {
+            var touch = Input.GetTouch(touchCounts - 1);
+            Debug.Log(touch.position.x);
+            if (touch.position.x > Screen.width / 2)
+            {
+                OnRightClicked();
+            }
+            else
+            {
+                OnLeftClicked();
+            }
+        }
+    }
+}
