3365eb919d24984a665a424e6b7565100d971a8b
Erdem ISGUDER <erdemisg@gmail.com>
2019-12-08 18:04:53 +0300

    max distance control were added

--- SideAttack/Assets/Scripts/Rogue/MovementComponent.cs
+++ SideAttack/Assets/Scripts/Rogue/MovementComponent.cs
@@ -2,7 +2,8 @@
 
 public class MovementComponent : MonoBehaviour
 {
-
+    [SerializeField]
+    private float maxDistance = 6f;
     public Transform targetTransform;
     public float speed = 1f;
     
@@ -13,8 +14,15 @@ public class MovementComponent : MonoBehaviour
             return;
         }
 
+        float distance = targetTransform.position.x - transform.position.x;
+
+        if(Mathf.Abs(distance) < maxDistance)
+        {
+            return;
+        }
+
         float step = speed * Time.deltaTime;
-        transform.rotation = Quaternion.Euler(0, targetTransform.position.x > transform.position.x ? 0 : 180, 0);
+        transform.rotation = Quaternion.Euler(0, distance > 0 ? 0 : 180, 0);
         transform.position = Vector2.MoveTowards(transform.position, targetTransform.position, step);
     }
 }
