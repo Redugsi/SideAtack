b5c1ba0d395cc43c76985935652ea6bef0dadcdd
Erdem ISGUDER <erdemisg@gmail.com>
2020-01-01 20:39:25 +0300

    Attack and die

---
+++ SideAttack/Assets/Scripts/Controller/Bot/Properties.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+public class Properties : MonoBehaviour
+{
+    public Transform pelvis;
+    public bool isDead = true;
+    private Animator animator;
+
+    public void SetReady()
+    {
+        if(pelvis == null) 
+        {
+            pelvis = transform.Find("Rogue_pelvis_01");
+        }
+
+        if(animator == null)
+        {
+            animator = GetComponent<Animator>();
+        }
+
+        gameObject.SetActive(true);
+        pelvis.transform.rotation = Quaternion.identity;
+        isDead = false;
+        animator.Play("Rogue_idle_01", -1, 0f);
+    }
+}
